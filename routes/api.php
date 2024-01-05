b584e1c7daaa1c1530c657486a002cd0e642dbb7
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-01-05 16:23:49 +0600

    category contorller and database schema design

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
+Route::apiResource('categories', CategoryController::class);
