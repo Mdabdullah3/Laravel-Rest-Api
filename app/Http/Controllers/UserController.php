7c318d36842faa9bea1c45727bd664a330fa0048
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-01-05 12:09:07 +0600

    users crud api created

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::all();
+    }
+    public function show($id)
+    {
+        return User::find($id);
+    }
+    public function store(Request $request)
+    {
+        return User::create($request->all());
+    }
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->update($request->all());
+        return $user;
+    }
+
+    public function destroy($id)
+    {
+        User::destroy($id);
+        return response()->json(['message' => 'User deleted successfully']);
+    }
+}
